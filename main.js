efc846216444450bcec3ced74c46e836ed2a65e2
Bagus Kurniawan <baguskurniawan999@gmail.com>
2020-04-10 21:57:17 +0700

    js core function

---
+++ main.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+
+  let _tmp_url = window.location;
+  let _base_url = _tmp_url .protocol + "//" + _tmp_url.host + "/" + _tmp_url.pathname.split('/')[1];
+  let _full_url = new URL(_tmp_url.href);
+
+  // load sidenav
+  $('.sidenav').load('components/_sidenav.html')
+
+  // handle url
+  let _pages = _full_url.searchParams.get('pages');
+  if (!_pages) {
+    _pages = 'home.html'
+    window.history.pushState('', 'Home', `${_base_url}?pages=${_pages}`);
+  }
+  $('.content').load(`pages/${_pages}`);
+
+  // handle click menu
+  $('body').on('click', '.list-group-item', function () {
+    $('.list-group-item').removeClass('active');
+    $(this).addClass('active');
+
+    let _href = $(this).data('href');
+    let _title = _href.split('.')[0];
+
+    $('.content').load(`pages/${_href}`);
+    window.history.pushState('', _title, `${_base_url}?pages=${_href}`);
+  });
+
+});
