c37fe022f1c358a9b2ceeb2b6106bd4e0a5e1c3c
Bagus Kurniawan <baguskurniawan999@gmail.com>
2020-04-19 20:28:28 +0700

    tidy up calculator function

--- assets/js/calculator.js
+++ assets/js/calculator.js
@@ -1,31 +1,15 @@
-function sum() {
+function count(param) {
+
+  let operators = {
+    '+': function (a, b) { return a+b  },
+    '-': function (a, b) { return a-b  },
+    '*': function (a, b) { return a*b  },
+    '/': function (a, b) { return a/b  },
+  }
+
   let num1 = document.getElementById('num1').value;
   let num2 = document.getElementById('num2').value;
   let result = document.getElementById('result');
 
-  result.value = parseFloat(num1) + parseFloat(num2);
-}
-
-function subtract() {
-  let num1 = document.getElementById('num1').value;
-  let num2 = document.getElementById('num2').value;
-  let result = document.getElementById('result');
-
-  result.value = parseFloat(num1) - parseFloat(num2);
-}
-
-function multiple() {
-  let num1 = document.getElementById('num1').value;
-  let num2 = document.getElementById('num2').value;
-  let result = document.getElementById('result');
-
-  result.value = parseFloat(num1) * parseFloat(num2);
-}
-
-function divide() {
-  let num1 = document.getElementById('num1').value;
-  let num2 = document.getElementById('num2').value;
-  let result = document.getElementById('result');
-
-  result.value = parseFloat(num1) / parseFloat(num2);
+  result.value = operators[param](parseFloat(num1), parseFloat(num2));
 }
